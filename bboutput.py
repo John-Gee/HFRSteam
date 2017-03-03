a30d167ea928e4d6c3b6d9d3315695e54fce1bda
John-Gee <john.ettedgui@gmail.com>
2017-03-03 09:35:45 -0800

    Fixed bbcode for no link

--- bboutput.py
+++ bboutput.py
@@ -6,8 +6,11 @@ def output_to_bb(games, file):
     bbcode = ''
     for gameName in sorted(games):
         if (games[gameName].store.link != ''):
-            bbcode = bbcode + '[url={0}]{1}[/url]{2}'.format(
-                games[gameName].store.link, gameName, os.linesep)
+            gamecode = '[url={0}]{1}[/url]'.format(
+                games[gameName].store.link, gameName)
+        else:
+            gamecode = gameName
+        bbcode = '{0}{1}{2}'.format(bbcode, gamecode, os.linesep)
 
     f = open(file, 'w')
     f.write(bbcode)
