8eff0253eefa5a897806d1c0059b9d42d41fda25
John-Gee <john.ettedgui@gmail.com>
2017-07-16 04:26:38 -0700

    replaced has_key by get as suggested by flyinpoulpus

--- cpu.py
+++ cpu.py
@@ -8,7 +8,7 @@ def get_number_of_cores():
         if (cores):
             return cores
     if (sys.platform.startswith('win32')):
-        if (os.environ.has_key("NUMBER_OF_PROCESSORS")):
+        if (os.environ.get("NUMBER_OF_PROCESSORS")):
             cores = int(os.environ["NUMBER_OF_PROCESSORS"])
             if (cores):
                 return cores
