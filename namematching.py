04c837ce6fcb8873eebc14e61565d52cb71fcc8a
FlyinPoulpus <mathieu.p.marquet@gmail.com>
2017-07-18 23:13:36 +0200

    Fixed a typo

--- namematching.py
+++ namematching.py
@@ -1,5 +1,5 @@
 import difflib
 
 
-def get_match(sentance, possibilities, n=1):
-    return difflib.get_close_matches(sentance, possibilities, n)
+def get_match(sentence, possibilities, n=1):
+    return difflib.get_close_matches(sentence, possibilities, n)
