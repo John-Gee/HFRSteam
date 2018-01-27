fb66543521491ca35ac37a9773db5111487f0299
John-Gee <john.ettedgui@gmail.com>
2018-01-27 06:34:51 -0800

    Refactored the code and skip unavailable games

--- bboutput.py
+++ bboutput.py
@@ -4,12 +4,14 @@ import os
 def output_to_bb(games, file):
     bbcode = ''
     for gameName in sorted(games):
+        if (not games[gameName].hfr.is_available):
+            continue
+
         if (games[gameName].store.link != ''):
-            gamecode = '[url={0}]{1}[/url]'.format(
-                games[gameName].store.link, gameName)
+            bbcode = '{0}[url={1}]{2}[/url]{3}'.format(
+                bbcode, games[gameName].store.link, gameName, os.linesep)
         else:
-            gamecode = gameName
-        bbcode = '{0}{1}{2}'.format(bbcode, gamecode, os.linesep)
+            bbcode = '{0}{1}{2}'.format(bbcode, gameName, os.linesep)
 
     f = open(file, 'w', encoding='utf8')
     f.write(bbcode)
