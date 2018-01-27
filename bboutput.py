6fab5e59c864602cd14a3482c85e0f15b7d23e8d
John-Gee <john.ettedgui@gmail.com>
2018-01-27 06:41:20 -0800

    Added the requirements and refactored for it

--- bboutput.py
+++ bboutput.py
@@ -3,15 +3,23 @@ import os
 
 def output_to_bb(games, file):
     bbcode = ''
-    for gameName in sorted(games):
-        if (not games[gameName].hfr.is_available):
+    requirements = ''
+    for gameName, game in sorted(
+        games.items(), key = lambda x: (x[1].hfr.requirements, x[0])):
+        if (not game.hfr.is_available):
             continue
 
-        if (games[gameName].store.link != ''):
+        if (game.hfr.requirements != requirements):
+            requirements = game.hfr.requirements
+            bbcode = '{0}{1}[b]{2}:[/b]{1}'.format(
+                bbcode, os.linesep, requirements, os.linesep)
+
+        if (game.store.link != ''):
             bbcode = '{0}[url={1}]{2}[/url]{3}'.format(
-                bbcode, games[gameName].store.link, gameName, os.linesep)
+                bbcode, game.store.link, gameName, os.linesep)
         else:
-            bbcode = '{0}{1}{2}'.format(bbcode, gameName, os.linesep)
+            bbcode = '{0}{1}{2}'.format(
+                bbcode, gameName, os.linesep)
 
     f = open(file, 'w', encoding='utf8')
     f.write(bbcode)
