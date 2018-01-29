52c42cf21cf087f88d0b23bd06771a54c46274c2
John-Gee <john.ettedgui@gmail.com>
2018-01-29 05:27:17 -0800

    Added a function to return the match score

--- namematching.py
+++ namematching.py
@@ -3,3 +3,7 @@ import difflib
 
 def get_match(sentence, possibilities, n=1):
     return difflib.get_close_matches(sentence, possibilities, n)
+
+
+def get_match_score(string1, string2):
+    return difflib.SequenceMatcher(None, string1, string2).ratio()
