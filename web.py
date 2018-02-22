9b4047da28250656371e6288cb1e224364c1fbc4
John-Gee <john.ettedgui@gmail.com>
2018-02-22 05:09:28 -0800

    Added a handling of too many redirect exception

--- web.py
+++ web.py
@@ -8,5 +8,8 @@ def get_utf8_web_page(url):
     jar = requests.cookies.RequestsCookieJar()
     jar.set('birthtime', '1')
     jar.set('mature_content', '1')
-    req = session.get(url, headers=headers, cookies=jar, allow_redirects=True)
-    return req.url, req.status_code, req.text
+    try:
+        req = session.get(url, headers=headers, cookies=jar, allow_redirects=True)
+        return req.url, req.status_code, req.text
+    except requests.exceptions.TooManyRedirects:
+        return None, None, None
