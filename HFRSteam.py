f04b4933c96bf447e76ef2457c577416221c4f75
John-Gee <john.ettedgui@gmail.com>
2018-02-23 06:19:12 -0800

    Cleaned up the argparse code and its output

--- HFRSteam.py
+++ HFRSteam.py
@@ -10,26 +10,44 @@ import htmloutput
 import utils
 
 
+class MyFormatter(argparse.ArgumentDefaultsHelpFormatter,
+                  argparse.MetavarTypeHelpFormatter):
+    def __init__(self, *args, **kwargs):
+        super(MyFormatter, self).__init__(*args, max_help_position=100,
+                                          width=80, **kwargs)
+
+
+    def _format_action_invocation(self, action):
+        if ((not action.option_strings) or (action.nargs == 0)):
+            return super()._format_action_invocation(action)
+
+        default = self._get_default_metavar_for_optional(action)
+        args_string = self._format_args(action, default)
+        return '{0} {1}'.format(', '.join(action.option_strings),
+                                args_string.upper())
+
+
 def main():
-    parser = argparse.ArgumentParser(description=('Parse the HFR list'
-                                     'of Steam games offered and convert it to '
-                                     'a nice bb/html page.'))
+    parser = argparse.ArgumentParser(description=('Parses the HFR list of Steam games'
+                                     ' offered and convert it to bb/html.'''),
+                            usage='%(prog)s [options]',
+                            formatter_class=MyFormatter,)
     parser.add_argument('-a', '--all', action='store_true', dest='all',
-                        help='Does not skip games that are not available')
+                        help='do not skip nonavailable games')
     parser.add_argument('-c', '--cache-only', action='store_true', dest='cacheonly',
-                        help='Does not query steam and only work on games in cache')
+                        help='do not query the store, cache-only')
     parser.add_argument('-d', '--dry-run', action='store_true', dest='dryrun',
-                        help='Performs a dry run, does not modify anything on disk')
+                        help='do not modify anything on disk')
     parser.add_argument('-i', '--ignore-cache', action='store_true', dest='ignorecache',
-                        help='Ignore the data stored in cache')
-    parser.add_argument('-l', '--list', dest='liste',
-                        help='Provide a text list to parse instead of parsing HFR')
+                        help='do not read the cache, but write to it')
+    parser.add_argument('-l', '--list', dest='liste', type=str, metavar='LIST',
+                        help='parse a text list instead of HFR')
     parser.add_argument('-m', '--matching-words', action='store_true', dest='matchingwords',
-                        help='Use a word matching algorithm to find a name matching in the steamdb (potentially wrong)')
-    parser.add_argument('-r', '--refresh-named', dest='game',
-                        help='Refresh the game listed, and all games not in cache')
+                        help='use approximate string matching')
+    parser.add_argument('-r', '--refresh', dest='game', type=str,
+                        help='refresh the games matching the string')
     parser.add_argument('-t', '--threads', dest='threads', default='0', type=int,
-                        help='Number of parallel threads to use for fetching the steam information.')
+                        help='number of parallel threads to use')
 
     options = parser.parse_args()
     if (options.liste == None):
