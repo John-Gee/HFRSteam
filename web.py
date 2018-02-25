dd8bc60b71bb5d999186fa5300be864327a4b013
John-Gee <john.ettedgui@gmail.com>
2018-02-25 14:35:23 -0800

    Cache requests

--- web.py
+++ web.py
@@ -1,7 +1,10 @@
+from cachecontrol import CacheControl
 import requests
 
 
-session = requests.Session()
+session        = requests.Session()
+cached_session = CacheControl(session)
+
 
 def get_utf8_web_page(url):
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -9,7 +12,15 @@ def get_utf8_web_page(url):
     jar.set('birthtime', '1')
     jar.set('mature_content', '1')
     try:
-        req = session.get(url, headers=headers, cookies=jar, allow_redirects=True)
+        req = cached_session.get(url, headers=headers,
+                                 cookies=jar, allow_redirects=True)
         return req.url, req.status_code, req.text
     except requests.exceptions.TooManyRedirects:
         return None, None, None
+
+
+if __name__ == '__main__':
+    url, status, page = get_utf8_web_page(
+        'https://forum.hardware.fr/hfr/JeuxVideo/'
+        'Achat-Ventes/gratuit-origin-download-sujet_171605_1.htm')
+    print(page)
