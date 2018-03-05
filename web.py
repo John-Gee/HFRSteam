32e74ee632867a19a1fc4cc0d82158490af0beb0
John-Gee <john.ettedgui@gmail.com>
2018-03-04 21:37:30 -0800

    Moved more static stuff out of the function + logging

--- web.py
+++ web.py
@@ -1,22 +1,31 @@
 from cachecontrol import CacheControl
 from cachecontrol.caches.file_cache import FileCache
+from cachecontrol import heuristics
+import logging
 import requests
 
 
-cached_session = CacheControl(requests.Session())
-                              #cache=FileCache('.webcache', forever=True))
-headers = {'User-Agent': 'Mozilla/5.0'}
-jar = requests.cookies.RequestsCookieJar()
-jar.set('birthtime', '1')
-jar.set('mature_content', '1')
+session = requests.Session()
+session.headers = {'User-Agent': 'Mozilla/5.0'}
+session.cookies = requests.cookies.RequestsCookieJar()
+session.cookies.set('birthtime', '1')
+session.cookies.set('mature_content', '1')
+session.allow_redirects = True
+
+cached_session = CacheControl(session,
+                              cache=FileCache('.webcache', forever=True),
+                              heuristic=heuristics.ExpiresAfter(days=20),
+                              cache_etags=False)
 
 
 def get_utf8_web_page(url):
     try:
-        req = cached_session.get(url, headers=headers,
-                                 cookies=jar, allow_redirects=True)
+        logging.debug('About to get the url: ' + url)
+        req = cached_session.get(url)
+        logging.debug('Got the url: ' + url)
         return req.url, req.status_code, req.text
     except requests.exceptions.TooManyRedirects:
+        logging.debug('TooManyRedirects for url: ' + url)
         return None, None, None
 
 
