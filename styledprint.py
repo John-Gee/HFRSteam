8732b1e38feaf642e3ce862917ff5849966ac937
John-Gee <john.ettedgui@gmail.com>
2018-03-06 22:25:46 -0800

    Added logging abilities

--- styledprint.py
+++ styledprint.py
@@ -1,5 +1,7 @@
 from colorama import Fore
 from enum import Enum
+import io
+import logging
 
 
 class Verbosity(Enum):
@@ -10,6 +12,7 @@ class Verbosity(Enum):
         if (self.__class__ is other.__class__):
             return self.value >= other.value
         return NotImplemented
+
 
 default_verbosity = Verbosity.Error
 
@@ -24,12 +27,19 @@ def print_styled(verbosity, color, text, *args):
         print(color + text + Fore.RESET, *args)
 
 
+def to_print(text, *args):
+    with io.StringIO() as sio:
+        print(text, *args, file=sio)
+        return sio.getvalue()
+
 def print_error(text, *args):
     print_styled(Verbosity.Error, Fore.RED, text, *args)
+    logging.error(to_print(text, *args))
 
 
 def print_info(text, *args):
     print_styled(Verbosity.Warning, Fore.YELLOW, text, *args)
+    logging.info(to_print(text, *args))
 
 
 def print_info_begin(text, *args):
@@ -42,3 +52,4 @@ def print_info_end(text, *args):
 
 def print_debug(text, *args):
     print_styled(Verbosity.Debug, Fore.CYAN, text, *args)
+    logging.debug(to_print(text, *args))
